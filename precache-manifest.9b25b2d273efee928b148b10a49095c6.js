2e2afd5919b8be78dc5bfc9ac5683af8d79de2b8
alfiandpratama <pratama.d.alfian@gmail.com>
2020-06-27 13:56:14 +0700

    Updates

---
+++ precache-manifest.9b25b2d273efee928b148b10a49095c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e6ebb4d41eaf92fcd87b840eb86b32",
+    "url": "/monsters-rolodex-redux/index.html"
+  },
+  {
+    "revision": "2aed80efcad3e001fa03",
+    "url": "/monsters-rolodex-redux/static/css/main.73d39cba.chunk.css"
+  },
+  {
+    "revision": "4c2978b2def2154cbcb6",
+    "url": "/monsters-rolodex-redux/static/js/2.52bc6e14.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monsters-rolodex-redux/static/js/2.52bc6e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aed80efcad3e001fa03",
+    "url": "/monsters-rolodex-redux/static/js/main.38a21726.chunk.js"
+  },
+  {
+    "revision": "a11273d10a23cd7c8516",
+    "url": "/monsters-rolodex-redux/static/js/runtime-main.ec291a6c.js"
+  }
+]);
